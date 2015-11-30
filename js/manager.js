78ec6f06e5147d4aeb3f4c37d65ad19cd23e324c
cmgomes <cmgomes.es@gmail.com>
2015-11-30 17:52:05 -0200

    Ajustes layout

---
+++ js/manager.js
@@ -0,0 +1,30 @@
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+$(document).ready(function(){
+    var page = getParameterByName('page');
+    
+    if(page == '' || page == undefined){
+        page = index;
+    }
+
+    $.ajax({
+        url: 'pages/' + page + '.html',
+        success: function(data){
+            $("#container").empty().html(data);
+        },
+        error: function(){
+            $.ajax({
+                url: 'pages/index.html',
+                success: function(data){
+                    $("#container").empty().html(data);
+                }
+            });
+        }
+    });
+
+});
