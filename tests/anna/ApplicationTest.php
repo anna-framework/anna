9219592f2d272d07ca308ea87ea51d48b9b0075c
cmgomes <cmgomes.es@gmail.com>
2015-12-03 12:06:05 -0200

    Ajustes no ambiente de teste

--- tests/anna/ApplicationTest.php
+++ tests/anna/ApplicationTest.php
@@ -1,12 +1,9 @@
 <?php
 
-namespace AnnaTests;
-
 class ApplicationTest extends PHPUnit_Framework_TestCase
 {
     public function testAssert()
     {
         $this->assetTrue(true);
     }
-    
 }
