5b4da163131d246ab822e8bbef4b1c770d3fd2d1
cmgomes <cmgomes.es@gmail.com>
2015-12-07 11:18:16 -0500

    Applied fixes from StyleCI

--- tests/anna/ResponseTest.php
+++ tests/anna/ResponseTest.php
@@ -4,30 +4,29 @@ use Anna\Response;
 
 class ResponseTest extends PHPUnit_Framework_TestCase
 {
+    public function setUp()
+    {
+        define('DS', DIRECTORY_SEPARATOR);
+        define('SYS_ROOT', dirname(dirname(__DIR__)).DS);
+        define('PS', PATH_SEPARATOR);
+        define('EOL', PHP_EOL);
+        define('ANNA_ROOT', __DIR__.DS);
 
-	public function setUp()
-	{
-		define('DS', DIRECTORY_SEPARATOR);
-		define('SYS_ROOT', dirname(dirname(__DIR__)).DS);
-		define('PS', PATH_SEPARATOR);
-		define('EOL', PHP_EOL);
-		define('ANNA_ROOT', __DIR__.DS);
+        chdir(SYS_ROOT);
+    }
 
-		chdir(SYS_ROOT);
-	}
+    /**
+     * @cover \Anna\Response
+     */
+    public function testResponse()
+    {
+        try {
+            $request = new Response();
+        } catch (Exception $e) {
+            $this->assertTrue(false, $e->getMessage());
+        }
 
-	/**
-	 * @cover \Anna\Response
-	 */
-	public function testResponse(){
-		try {
-			$request = new Response();
-		} catch (Exception $e){
-			$this->assertTrue(false, $e->getMessage());
-		}
-
-		//instance test
-		$this->assertEquals(Response::class, get_class($request));
-	}
-
+        //instance test
+        $this->assertEquals(Response::class, get_class($request));
+    }
 }
