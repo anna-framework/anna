2e07d7787c7b2d374cca8becc83b92497e025171
cmgomes <cmgomes.es@gmail.com>
2015-12-08 09:01:16 -0200

    Ajustes nos testes

--- tests/anna/ViewTest.php
+++ tests/anna/ViewTest.php
@@ -6,23 +6,27 @@ class ViewTest extends PHPUnit_Framework_TestCase
 {
     public function setUp()
     {
-        define('DS', DIRECTORY_SEPARATOR);
+        if (!defined('DS')) {
+    		define('DS', DIRECTORY_SEPARATOR);
+    	}
 
-        define('SYS_ROOT', dirname(dirname(__DIR__)).DS);
+    	if (!defined('SYS_ROOT')) {
+    		define('SYS_ROOT', dirname(dirname(__DIR__)).DS);
+    	}
+
+    	if (!defined('PS')) {
+    		define('PS', PATH_SEPARATOR);
+    	}
+
+    	if (!defined('EOL')) {
+    		define('EOL', PHP_EOL);
+    	}
+
+    	if (!defined('ANNA_ROOT')) {
+    		define('ANNA_ROOT', __DIR__.DS);
+    	}
 
         chdir(SYS_ROOT);
-
-        if (!defined('PS')) {
-            define('PS', PATH_SEPARATOR);
-        }
-
-        if (!defined('EOL')) {
-            define('EOL', PHP_EOL);
-        }
-
-        if (!defined('ANNA_ROOT')) {
-            define('ANNA_ROOT', __DIR__.DS);
-        }
     }
 
     /**
